6b71329ee79524878d8e1a6bba3cb4b6247402e7
Wang Wei14(上海_技术部_平台保障_王伟) <wangwei14@yhd.com>
2016-11-30 10:12:03 +0800

    test

---
+++ src/main/java/proxyAndAop/dynamicProxy/TicketService.java
@@ -0,0 +1,17 @@
+package proxyAndAop.dynamicProxy; 
+/** 
+ * 售票服务接口  
+ * @author louluan 
+ */  
+public interface TicketService {  
+  
+    //售票  
+    public void sellTicket();  
+      
+    //问询  
+    public void inquire();  
+      
+    //退票  
+    public void withdraw();  
+      
+}  
