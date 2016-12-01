b8627a622acf457785cd579e83e1e19a1637f05c
Wang Wei14(上海_技术部_平台保障_王伟) <wangwei14@yhd.com>
2016-12-01 10:59:11 +0800

    test

--- src/main/java/btraceTest/BtraceTest.java
+++ src/main/java/btraceTest/BtraceTest.java
@@ -16,6 +16,7 @@ public class BtraceTest {
 					System.out.println("main");
 					try {
 						new BtraceTest().methodA();
+						new BtraceTest().methodB();
 						Thread.sleep(1000);
 						interrupt();
 					} catch (InterruptedException e) {
@@ -28,8 +29,17 @@ public class BtraceTest {
 	}
 	
 	public void methodA() {
+		String s = "";
 		for(int i=0; i<3; i++) {			
 			System.out.println( "Thread: " + Thread.currentThread().getName() + ", i :" + i );	
+			for( int j =0; j<10000; j++) {
+				s +=  ("jjjjjjjjjjjjjjjjjjjjjjjjj = " + j);
+			}
 		}		
+	}
+	
+	public void methodB() {
+	   System.out.println( "methodB ");
+	   throw  new RuntimeException("ttttt");
 	}
 }
