2b77fd29cf91fd68b25c7e1cfe942aa5e71d3f0f
3w <www6v@126.com>
2018-07-26 18:40:14 +0800

    test

---
+++ src/main/java/javacore/concurrent/SynchronizationOnMutableFieldDemo_Right.java
@@ -0,0 +1,63 @@
+package javacore.concurrent;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+/**
+ * @author Jerry Lee (oldratlee at gmail dot com)
+ */
+public class SynchronizationOnMutableFieldDemo_Right {
+    static final int ADD_COUNT = 10000;
+
+    static class Listener {
+        // stub class
+    }
+
+//    private volatile  List<Listener> listeners = new CopyOnWriteArrayList<Listener>();
+    private final  List<Listener> listeners = new ArrayList<Listener>();
+
+    public static void main(String[] args) throws Exception {
+        SynchronizationOnMutableFieldDemo_Right demo = new SynchronizationOnMutableFieldDemo_Right();
+
+        Thread thread1 = new Thread(demo.getConcurrencyCheckTask());
+        thread1.start();
+        Thread thread2 = new Thread(demo.getConcurrencyCheckTask());
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        int actualSize = demo.listeners.size();
+        int expectedSize = ADD_COUNT * 2;
+        if (actualSize != expectedSize) {
+            // 在我的开发机上，几乎必现！（简单安全的解法：final List字段并用并发安全的List，如CopyOnWriteArrayList）
+            System.err.printf("Fuck! Lost update on mutable field! actual %s expected %s.\n", actualSize, expectedSize);
+        } else {
+            System.out.println("Emm... Got right answer!!");
+        }
+    }
+
+    public void addListener(Listener listener) {
+        synchronized (listeners) {
+//            List<Listener> results = new ArrayList<Listener>(listeners);
+//            results.add(listener);
+//            listeners = results;
+            listeners.add(listener);
+        }
+    }
+
+    ConcurrencyCheckTask getConcurrencyCheckTask() {
+        return new ConcurrencyCheckTask();
+    }
+
+    private class ConcurrencyCheckTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("ConcurrencyCheckTask started!");
+            for (int i = 0; i < ADD_COUNT; ++i) {
+                addListener(new Listener());
+            }
+            System.out.println("ConcurrencyCheckTask stopped!");
+        }
+    }
+}
