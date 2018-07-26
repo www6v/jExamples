72ffb5c837621f87741fbb14fac146a156e99df1
3w <www6v@126.com>
2018-07-26 18:39:41 +0800

    test

--- src/main/java/spring/proxy/factory/bean/MyBeforeAdvice.java
+++ src/main/java/spring/proxy/factory/bean/MyBeforeAdvice.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Method;
 import org.springframework.aop.MethodBeforeAdvice;
 
 public class MyBeforeAdvice implements MethodBeforeAdvice{
-    @Override
 
     public void before(Method method, Object[] args, Object target)
 
