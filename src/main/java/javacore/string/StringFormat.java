a6a0af73beca5c91a8c10e20a647fca18b0587f0
3w <www6v@126.com>
2018-08-08 18:23:10 +0800

    btrace

---
+++ src/main/java/javacore/string/StringFormat.java
@@ -0,0 +1,13 @@
+package javacore.string;
+
+/**
+ * Created by wangwei110 on 2018/5/7.
+ */
+public class StringFormat {
+    public static void main(String arg[]) {
+        String fdbName = "features.fdb";
+        String f = String.format("transfdb.sh %s", fdbName);
+
+        System.out.println(f);
+    }
+}
