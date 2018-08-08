eab37e72fcdd4df0c5863bc6477572649b98d3b0
3w <www6v@126.com>
2018-08-08 18:23:44 +0800

    aop

--- src/main/java/spring/aop/TicketServiceAroundAdvice.java
+++ src/main/java/spring/aop/TicketServiceAroundAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.aop.aspectj.AspectJAroundAdvice;
  * Created by louis on 2016/4/15. 
  */  
 public class TicketServiceAroundAdvice implements MethodInterceptor {  
-    @Override  
+
     public Object invoke(MethodInvocation invocation) throws Throwable {  
         System.out.println("AROUND_ADVICE:BEGIN....");  
         Object returnValue = invocation.proceed();  
