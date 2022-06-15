7b22b1c5297be27a339f1acac0d5effe976b122f
www6v <www6v@tuhuadmindeMacBook-Air.local>
2022-06-15 17:26:31 +0800

    qiniu

---
+++ src/main/java/algorithm/qiniu/Solution.java
@@ -0,0 +1,17 @@
+package algorithm.qiniu;
+
+/// 删除数组中的重复项
+public class Solution {
+
+    public int removeDuplicatedElem(String[] nums) {
+        int n = nums.length;
+        int  j = 0;
+        for (int i = 0; i < n; i++) {
+            if(nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+
+        return j+1;
+    }
+}
