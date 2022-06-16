8275d98ab2bea95c95759ad97cb4476b6c69bc56
www6v <www6v@tuhuadmindeMacBook-Air.local>
2022-06-16 15:21:59 +0800

    algo

---
+++ src/main/java/algorithm/company/dataVisor/Solution.java
@@ -0,0 +1,29 @@
+package algorithm.company.dataVisor;
+
+public class Solution {
+    public void methodA(int[] nums1, int m) {
+
+    }
+
+    class ListNode {
+        public int value;
+        public ListNode next;
+
+        public ListNode(int data) {
+            this.value = data;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
