13641eb3dc6049675d0d719e471f6650102ac8e5
www6v <www6v@tuhuadmindeMacBook-Air.local>
2022-06-19 17:09:41 +0800

    algot

---
+++ src/main/java/algorithm/company/nio/Solution1.java
@@ -0,0 +1,26 @@
+package algorithm.company.nio;
+
+
+/**
+ * 有一些文件路径，需要转换成一棵树，根节点是root
+ *
+ * /data/a
+ * /data/a/b
+ * /var/a/b
+ *
+ *        root
+ * |                  |
+ * data           var
+ * |                  |
+ * a                 a
+ * |                  |
+ * b                 b
+ *
+ * 要求：
+ * 定义节点数据结构
+ * 完成转换函数
+ */
+public class Solution1 {
+    public void methodA(int[] nums1, int m) {
+    }
+}
